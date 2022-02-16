1e9fd3609c5235c54f030a1ab4e16c593cb08729
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-02-15 22:34:17 -0300

    Curso.

---
+++ sql-no-nodejs-com-sequelize/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'example',
+    database: 'sqlnode',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};

0e609da7f5aa5c4fb93e220271e1757ae1acd67c
Alessandro Bispo dos Santos <alessandro@bispo.eti.br>
2022-03-29 10:20:54 -0300

    Azure Blob Storage Example
    * Loading Shared Access Signature from .env file.

--- azure-blob-storage-example/src/components/UploadFile.tsx
+++ azure-blob-storage-example/src/components/UploadFile.tsx
@@ -18,7 +18,7 @@ function UploadFile() {
 
     const onFileUpload = async () => {
         const formData = new FormData();
-        const containerClient = new ContainerClient("http://localhost:10000/devstoreaccount1/files?sv=2018-03-28&st=2022-03-29T00%3A22%3A01Z&se=2022-03-30T00%3A22%3A01Z&sr=c&sp=racwdl&sig=84DLRJZQoeRbkOby6lGbjIAUzDwbnFfiww23wj%2BqM0U%3D")
+        const containerClient = new ContainerClient(`http://localhost:10000/devstoreaccount1/files?${process.env.REACT_APP_AZURE_CONTAINER_SHARED_ACCESS_SIGNATURE}`)
 
         if (selectedFile) {
             formData.append(
